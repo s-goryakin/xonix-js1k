0405e78c20ca7fa30debca31dce1e8e8bee471ba
sergey.goryakin <sergey.goryakin@gmail.com>
2011-03-15 09:53:56 +0000

    New positions calculation

--- game.js
+++ game.js
@@ -5,13 +5,28 @@ a.canvas.width = wi;
 a.canvas.height = he;
 x=100;
 y=100;
-a.fillText("@", x, y);
+a.fillText("x", x, y);
 w=window;
+
+function changeObjectPosition(o, x1, y1, x2, y2)
+{
+	// Delete old item first
+	a.fillStyle = "#fff";
+	// Less filling not fully deleting old text :(
+	a.fillText(o, x1, y1);
+	a.fillText(o, x1, y1);
+	a.fillText(o, x1, y1);
+	a.fillText(o, x1, y1);
+	a.fillText(o, x1, y1);
+	a.fillText(o, x1, y1);
+
+	// Draw new item
+	a.fillStyle = "#000";
+    a.fillText(o, x2, y2);
+}
+
 w.addEventListener('keyup', function(e) {
 	kc=e.keyCode
-    if(kc^37)x+=s;
-    if(kc^38)y+=s;
-    if(kc^39)x-=s;
-    if(kc^40)y-=s;
-    a.fillText("@", x, y);
+	x1=x;y1=y;
+	changeObjectPosition("x",x1,y1,x+=s*((kc==39)-(kc==37)),y+=s*((kc==40)-(kc==38)));
 });
