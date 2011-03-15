06c5537502344a79319610cffd783240c6bbc393
slimusgm <slimusgm@gmail.com>
2011-03-15 05:43:50 +0000

    you may change @ coords with left, right, up and down keys. please see it

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log('It works');
+wi=500;
+he=500;
+s=10;
+a.canvas.width = wi;
+a.canvas.height = he;
+x=100;
+y=100;
+a.fillText("@", x, y);
+w=window;
+w.addEventListener('keyup', function(e) {
+    if(e.keyCode^37)x+=s;
+    if(e.keyCode^38)y+=s;
+    if(e.keyCode^39)x-=s;
+    if(e.keyCode^40)y-=s;
+    a.fillText("@", x, y);
+});
